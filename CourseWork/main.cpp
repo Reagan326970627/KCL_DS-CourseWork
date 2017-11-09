e0e0c7781cf7357c90fad72b8afac4d9407de4eb
李根 <reagan@ligendeMacBook-Pro.local>
2017-11-09 13:12:21 +0000

    Initial Commit

---
+++ CourseWork/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CourseWork
+//
+//  Created by 李根 on 2017/11/9.
+//  Copyright © 2017年 李根. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
